daa8a3a4ce278efc3ab60bb6e8f4dbe18d7c4325
Zapihra <iida.vaaja@student.lut.fi>
2023-07-12 17:11:03 +0300

    pictures don't show

--- src/index.js
+++ src/index.js
@@ -1,10 +1,59 @@
-import "./styles.css";
+//import "./styles.css";
+import Phaser from "phaser";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+let game;
+
+const gameOptions = {
+  dudeGravity: 800,
+  dudeSpeed: 300
+}
+
+
+window.onload = function() {
+  let gameConfig = {
+    type: Phaser.AUTO,
+    backgroundColor: "#336633",
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 800,
+      height: 1000,
+    },
+    pixelArt:true,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: {
+          y: 0
+        }
+      }
+    },
+    scene: PlayGame
+  }
+  game = new Phaser.Game(gameConfig);
+  window.focus();
+
+}
+
+class PlayGame extends Phaser.Scene {
+  preload() {
+    this.load.image("ground", "assets/platform.png");
+    
+    this.load.spritesheet("player", "assets/character.png", 
+    {frameWidth: 32, frameHeight: 32});
+    //players license https://craftpix.net/file-licenses/
+  }
+
+  create() {
+    this.groundGroup = this.physics.add.group({
+      immovable: true,
+      allowGravity: false
+    })
+
+    for (let i = 0; i < 20 ; i++) {
+      this.groundGroup.create(Phaser.Math.Between(0, 
+        game.config.width), Phaser.Math.Between(0, 
+          game.config.height), "ground");
+    }
+  }
+}
