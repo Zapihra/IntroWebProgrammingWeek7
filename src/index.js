cf3b7aacd8b33559f76d234a1d13f85faed6988f
Zapihra <iida.vaaja@student.lut.fi>
2023-07-18 20:27:48 +0300

    final game

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-//import "./styles.css";
+//used the video in course material and talks with Viia Mäntymäki and Ida Kirveskoski
+//trying to solve the pictures not showing problem
 import Phaser from "phaser";
 import ground from "./assets/platform.png";
 import player from "./assets/character.png";
 import coin from "./assets/coin.png"
+import bCoin from "./assets/coinblue.png"
+import spike from "./assets/spike.png"
+import heartPix from "./assets/heartPix.png"
 //https://stackoverflow.com/questions/66878947/image-is-not-getting-added-to-the-scene-phaser-3-5
 // having the pictures show in the web browser
 
@@ -45,14 +49,21 @@ class PlayGame extends Phaser.Scene {
   constructor() {
     super("PlayGame");
     this.score = 0;
+    this.heart = 3;
   }
 
   preload() {
     this.load.image("ground", ground);
     this.load.image("coin", coin);
+    this.load.image("blue", bCoin);
+    this.load.image("spike", spike);
+    this.load.image("heart", heartPix);
     this.load.spritesheet("player", player, 
     {frameWidth: 32, frameHeight: 48});
-    //players license https://craftpix.net/file-licenses/
+    //coin from https://clipground.com/pics/get
+    //blue coin just colored to blue
+    // heart from https://pixabay.com/fi/illustrations/pixel-syd%C3%A4n-syd%C3%A4n-pikseli%C3%A4-symboli-2779422/
+    //spikes were made by me
   }
 
   create() {
@@ -62,6 +73,8 @@ class PlayGame extends Phaser.Scene {
     })
     let coin2 = this.add.image(16, 16,"coin");
     coin2.setScale(0.08);
+    let hearted = this.add.image(16, 48, "heart");
+    hearted.setScale(0.05)
 
     for (let i = 0; i < 20 ; i++) {
       this.groundGroup.create(Phaser.Math.Between(0, 
@@ -75,10 +88,22 @@ class PlayGame extends Phaser.Scene {
     this.coinGroup = this.physics.add.group({});
     this.physics.add.collider(this.coinGroup, this.groundGroup);
 
+    this.bCoinGroup = this.physics.add.group({});
+    this.spikes = this.physics.add.group({});
+    this.physics.add.collider(this.spikes, this.groundGroup);
+
+
     this.physics.add.overlap(this.player, this.coinGroup, 
       this.collectCoin, null, this);
 
+    this.physics.add.overlap(this.player, this.bCoinGroup, 
+      this.collectCoinBlue, null, this);
+
+      this.physics.add.overlap(this.player, this.spikes, 
+        this.spikesHurt, null, this);
+
     this.scoreText = this.add.text(32, 2, "0", {fontSize: "30px", fill: "#ffffff"})
+    this.heartText = this.add.text(32, 35, "3", {fontSize: "30px", fill: "#ffffff"})
 
     this.cursors = this.input.keyboard.createCursorKeys();
 
@@ -126,13 +151,37 @@ class PlayGame extends Phaser.Scene {
 
       this.coinGroup.setVelocityY(gameOptions.dudeGravity);
     }
-  
-  
+    
+    if(Phaser.Math.Between(0,0.3)) {
+      let coins = this.bCoinGroup.create(Phaser.Math.Between(0, 
+        game.config.width), Phaser.Math.Between(100, 
+          400), "blue");
+      coins.setScale(0.08);
+    }
+
+    if(Phaser.Math.Between(0,0.2)) {
+      this.spikes.create(Phaser.Math.Between(0, 
+        game.config.width), Phaser.Math.Between(100, 
+          300), "spike");
+      this.spikes.setVelocityY(gameOptions.dudeGravity /10)
+    }
+  }
+
+  spikesHurt(player, spikes) {
+    spikes.disableBody(false, true);
+    this.heart -= 1;
+    this.heartText.setText(this.heart)
   }
 
   collectCoin(player, coins) {
     coins.disableBody(true, true);
     this.score += 1
+    this.scoreText.setText(this.score);
+  }
+
+  collectCoinBlue(player, coinsb) {
+    coinsb.disableBody(true, true);
+    this.score += 2
     this.scoreText.setText(this.score);
   }
 
@@ -154,9 +203,10 @@ class PlayGame extends Phaser.Scene {
       this.player.body.velocity.y = -gameOptions.dudeGravity / 1.6;
     }
 
-    if(this.player.y > game.config.height || this.player.y < 0) {
+    if(this.player.y > game.config.height || this.player.y < 0 || this.heart == 0) {
       this.scene.start("PlayGame")
+      this.score = 0;
+      this.heart = 3;
     }
-
   }
 }
